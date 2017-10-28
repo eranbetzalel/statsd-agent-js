eb27c9ae407fe168b6405c4e5b414878a4c07231
Roman Valihura <roman.valihura@gmail.com>
2017-10-28 21:06:20 +0300

    Remove memory used as cache from metrics (#1)
    * do script intependent from pwd
    
    * memorystat without cache
    
    * change var to const
    
    * calculare usage memory usage as htop
    
    * split memory monitors to defalt and unix

--- src/config.js
+++ src/config.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const fs = require('fs-extra');
+const path = require('path');
 
 let customConfig;
 
 function loadCustomConfiguration() {
-    if (fs.existsSync('./config.custom.js')) {
+    if (fs.existsSync(__dirname + '/config.custom.js')) {
         console.log('Loading custom configuration...');
 
         customConfig = require('./config.custom');
     } else {
         console.log('Creating custom configuration file...');
 
-        fs.copySync('./config.default.js', './config.custom.js');
+        fs.copySync(__dirname + '/config.default.js', __dirname + '/config.custom.js');
 
         loadCustomConfiguration();
     }
